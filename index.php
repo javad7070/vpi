70cfe0c05a940ca693d9a228e7ca5f1ee09cdbad
Mr.javad <javadmohamadi70001377@gmail.com>
2017-01-16 18:06:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1296,7 +1296,7 @@ https://telegram.me/sscapachebot
                         ]);
                         $t = time().'.jpg';
                         $furl = 'http://bots.mhr-developer.com/images/'.$t;
-                        $fule = 'https://api.telegram.org/file/bot188540245:AAGGTpBT2UIDeuvRMfxq54KsrJsi05URSHI/'.trim($imag->result->file_path,"\\/");
+                        $fule = 'https://api.telegram.org/file/bot267540431:AAE0tklInbn6iMyRgNjRICwrmNq0iiwQSaw/'.trim($imag->result->file_path,"\\/");
                         $file_ext = end(explode(".",$fule));
                         if($file_ext == "jpg" || $file_ext == "jpeg") {
                             file_put_contents("/var/www/html/images/".$t, fopen($fule, 'r'));
